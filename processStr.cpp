7ab18fe54c9d847e62d188ea1e5660465967a675
Sudheer Yadav <sudheer01yadav2005@gmail.com>
2026-06-17 13:44:06 +0530

    Create processStr.cpp

---
+++ processStr.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string res = "";
+        for(char & ch : s){
+            if(ch == '*' ){
+                if( res.size() > 0)
+                   res.pop_back();
+            }
+            else if(ch == '#'){
+                res = res + res;
+            }
+            else if(ch == '%'){
+                reverse(res.begin() , res.end());
+            }
+            else {
+                res += ch;
+            }
+        }
+        return res;
+    }
+};
